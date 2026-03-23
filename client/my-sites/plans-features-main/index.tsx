b3dd1f490511fa4292b50109e8d1cd586244236a
Veselin Nikolov <veselin@automattic.com>
2026-03-23 13:16:34 +0200

    Create the new Feature Gating experiment based on Var4
    Add var4_1

--- client/my-sites/plans-features-main/index.tsx
+++ client/my-sites/plans-features-main/index.tsx
@@ -391,11 +391,8 @@ const PlansFeaturesMain = ( {
 		isLoading: isLoadingDifferentiatorsExperiment,
 		showDifferentiatorHeader,
 		variant: differentiatorsVariant,
-		useVar1Features,
-		useVar3Features,
 		useVar4Features,
-		useVar5Features,
-		isVar1dVariant,
+		useVar41MorePremiumFeatures,
 		isVar4Variant,
 		isExperimentVariant,
 	} = usePlanDifferentiatorsExperiment( { flowName, isInSignup, siteId } );
@@ -477,11 +474,8 @@ const PlansFeaturesMain = ( {
 		reflectStorageSelectionInPlanPrices: true,
 		isInSignup,
 		useLongSetFeatures: useVar4Features,
-		useLongSetStackedFeatures: useVar3Features,
-		useShortSetStackedFeatures: useVar1Features,
-		useVar5Features,
+		useVar41MorePremiumFeatures,
 		isExperimentVariant,
-		isVar1dVariant,
 	} );
 
 	// we need only the visible ones for features grid (these should extend into plans-ui data store selectors)
@@ -506,11 +500,8 @@ const PlansFeaturesMain = ( {
 		term,
 		reflectStorageSelectionInPlanPrices: true,
 		useLongSetFeatures: useVar4Features,
-		useLongSetStackedFeatures: useVar3Features,
-		useShortSetStackedFeatures: useVar1Features,
-		useVar5Features,
+		useVar41MorePremiumFeatures,
 		isExperimentVariant,
-		isVar1dVariant,
 	} );
 
 	// when `deemphasizeFreePlan` is enabled, the Free plan will be presented as a CTA link instead of a plan card in the features grid.
@@ -787,7 +778,7 @@ const PlansFeaturesMain = ( {
 		featureGroupMapForFeaturesGrid = getWooExpressFeaturesGroupedForFeaturesGrid();
 	} else if ( intent === 'plans-wordpress-hosting' ) {
 		featureGroupMapForFeaturesGrid = getWordPressHostingFeaturesGroupedForFeaturesGrid();
-	} else if ( useVar3Features || useVar4Features || useVar1Features || useVar5Features ) {
+	} else if ( useVar4Features || useVar41MorePremiumFeatures ) {
 		// Experiment: stacked variants should render a single, ordered list (no grouping),
 		// otherwise features get scattered across groups causing gaps and can be filtered out.
 		const featureGroups = getPlanFeaturesGroupedForFeaturesGrid();
@@ -981,7 +972,7 @@ const PlansFeaturesMain = ( {
 										enableTermSavingsPriceDisplay={ enableTermSavingsPriceDisplay }
 										showSimplifiedBillingDescription={ isInSignup }
 										showBillingDescriptionForIncreasedRenewalPrice={ renewalPricingVariation }
-										isVar1dVariant={ isVar1dVariant }
+										isVar1dVariant={ false }
 										isVar4Variant={ isVar4Variant }
 										isExperimentVariant={ isExperimentVariant }
 									/>
