efc4c105db04b1664c4187d4a1f2f9a5273e7832
Veselin Nikolov <veselin@automattic.com>
2026-03-24 14:31:04 +0200

    Rename prop responsible for showing pills

--- client/my-sites/plans-features-main/index.tsx
+++ client/my-sites/plans-features-main/index.tsx
@@ -394,7 +394,7 @@ const PlansFeaturesMain = ( {
 		useVar4Features,
 		useVar41MorePremiumFeatures,
 		useVar42NoAiFeatures,
-		isFocusedPremiumVariant,
+		showPricingDifferentiationFeaturePills,
 		isVar4Variant,
 		isExperimentVariant,
 	} = usePlanDifferentiatorsExperiment( { flowName, isInSignup, siteId } );
@@ -478,7 +478,7 @@ const PlansFeaturesMain = ( {
 		useLongSetFeatures: useVar4Features,
 		useVar41MorePremiumFeatures,
 		useVar42NoAiFeatures,
-		isFocusedPremiumVariant,
+		showPricingDifferentiationFeaturePills,
 		isExperimentVariant,
 	} );
 
@@ -506,7 +506,7 @@ const PlansFeaturesMain = ( {
 		useLongSetFeatures: useVar4Features,
 		useVar41MorePremiumFeatures,
 		useVar42NoAiFeatures,
-		isFocusedPremiumVariant,
+		showPricingDifferentiationFeaturePills,
 		isExperimentVariant,
 	} );
 
